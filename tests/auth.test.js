e11b2495b3fbdae9aa490043065d8d426b612001
Durlabh Jain <durlabh@gmail.com>
2021-12-09 19:48:28 -0500

    fix: test-cases for auth tests

--- tests/auth.test.js
+++ tests/auth.test.js
@@ -4,44 +4,47 @@ import { Buffer } from 'buffer';
 
 const { BearerAuth, BasicAuth } = httpAuth;
 
-const bearerAuth = new BearerAuth({
-    requestOptions: {
-        form: {
-            grant_type: process.env.TOKEN_GRANT_TYPE,
-            username: process.env.TOKEN_USERNAME,
-            password: process.env.TOKEN_PASSWORD,
+test('BearerAuth', async () => {
+    const bearerAuth = new BearerAuth({
+        requestOptions: {
+            form: {
+                grant_type: process.env.TOKEN_GRANT_TYPE,
+                username: process.env.TOKEN_USERNAME,
+                password: process.env.TOKEN_PASSWORD,
+            }
+        },
+        token: 'dummy',
+        url: process.env.TOKEN_URL,
+        tokenKey: process.env.TOKEN_KEY
+    });
+
+    let result = await util.request({
+        authentication: bearerAuth,
+        method: 'POST',
+        url: process.env.UPLOAD_URL,
+        body: () => {
+            return util.toFormData({
+                "file": Buffer.from(JSON.stringify("{}"))
+            })
         }
-    },
-    token: 'dummy',
-    url: process.env.TOKEN_URL,
-    tokenKey: process.env.TOKEN_KEY
+    });
+    expect(result.statusCode).toBe(200);
 });
 
-let result = await util.request({
-    authentication: bearerAuth,
-    method: 'POST',
-    url: process.env.UPLOAD_URL,
-    body: () => {
-        return util.toFormData({
-            "file": Buffer.from(JSON.stringify("{}"))
-        })
-    }
+test('basicAuth', async () => {
+    const basicAuth = new BasicAuth({
+        username: process.env.BASIC_USERNAME,
+        password: process.env.BASIC_PASSWORD
+    });
+    
+    const result = await util.request({
+        authentication: basicAuth,
+        method: 'POST',
+        url: process.env.BASIC_AUTH_TEST_URL,
+        form: {
+            "action": 'list'
+        }
+    });
+
+    expect(result.statusCode).toBe(200);
 });
-
-console.log({ statusCode: result.statusCode, body: result.body });
-
-const basicAuth = new BasicAuth({
-    username: process.env.BASIC_USERNAME,
-    password: process.env.BASIC_PASSWORD
-});
-
-result = await util.request({
-    authentication: basicAuth,
-    method: 'POST',
-    url: process.env.BASIC_AUTH_TEST_URL,
-    form: {
-        "action": 'list'
-    }
-});
-
-console.log({ statusCode: result.statusCode, body: result.body });
