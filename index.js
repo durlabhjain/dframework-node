588874e8d03ac6ce379460393e39e0d188499124
Durlabh Jain <durlabh@gmail.com>
2022-03-28 13:42:50 -0400

    feature: elasticsearch sql query and adapters

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ import mssql from 'mssql';
 import Azure from './lib/azure.js';
 import util from './lib/util.js';
 import httpAuth from './lib/http-auth/index.js';
-
+import Elastic from './lib/elastic.js';
+import adapters from './lib/adapters/index.js';
 
 export default Framework;
 
-export { mssql, Azure, util, httpAuth };
+export { mssql, Azure, util, httpAuth, Elastic, adapters };
