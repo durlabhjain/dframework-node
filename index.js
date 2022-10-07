89d5c605f569bcf2a8df027d06a558aaf5fe6301
deveshspraxa <43368796+deveshspraxa@users.noreply.github.com>
2022-10-08 00:56:40 +0530

    Logger Added

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ import util from './lib/util.js';
 import httpAuth from './lib/http-auth/index.js';
 import Elastic from './lib/elastic.js';
 import adapters from './lib/adapters/index.js';
+import logger from './lib/logger.js';
+import appConfig from './lib/appConfig.js';
 
 export default Framework;
 
-export { mssql, Azure, util, httpAuth, Elastic, adapters };
+export { mssql, Azure, util, httpAuth, Elastic, adapters, logger, appConfig };
