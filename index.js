3a76597ec09082936f48fd800a6ff1776e14fc6d
Tushar Singh <tushar.singh@spraxa.com>
2024-01-31 20:07:12 +0530

    Enhancement: Elastic Search Backend Changes.

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import appConfig from './lib/appConfig.js';
 import lookup from './lib/business/lookup.mjs';
 import { sqlErrorMapper } from './lib/business/error-mapper.mjs';
 import BusinessBase from './lib/business/business-base.mjs';
+import responseTransformer from './lib/middleware/response-transformer.mjs';
 export default Framework;
 
-export { mssql, Azure, util, httpAuth, Elastic, adapters, logger, appConfig, lookup, sqlErrorMapper, BusinessBase };
+export { mssql, Azure, util, httpAuth, Elastic, adapters, logger, appConfig, lookup, sqlErrorMapper, BusinessBase, responseTransformer };
