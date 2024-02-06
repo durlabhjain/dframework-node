784b44c63fbc77e4ebb862e51ebbe09e5292f3c7
Tushar Singh <tushar.singh@spraxa.com>
2024-02-06 14:04:16 +0530

    Enhancement: PR review Points from Pramod sir.

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ import lookup from './lib/business/lookup.mjs';
 import { sqlErrorMapper } from './lib/business/error-mapper.mjs';
 import BusinessBase from './lib/business/business-base.mjs';
 import responseTransformer from './lib/middleware/response-transformer.mjs';
+import ElasticBusinessBase from './lib/business/elastic-business-base.mjs';
 export default Framework;
 
-export { mssql, Azure, util, httpAuth, Elastic, adapters, logger, appConfig, lookup, sqlErrorMapper, BusinessBase, responseTransformer };
+export { mssql, Azure, util, httpAuth, Elastic, adapters, logger, appConfig, lookup, sqlErrorMapper, BusinessBase, responseTransformer, ElasticBusinessBase };
