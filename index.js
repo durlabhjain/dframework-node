f96f9c824f3b661a268cd8e5ee61e230ed6699f2
Rohan Kumar <rohan.kumar@spraxa.com>
2024-05-24 16:00:56 +0530

    MySql Changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import Framework from './lib/index.js';
 import mssql from 'mssql';
+import mysql from 'mysql2/promise'
 import Azure from './lib/azure.js';
 import util from './lib/util.js';
 import httpAuth from './lib/http-auth/index.js';
@@ -14,4 +15,4 @@ import responseTransformer from './lib/middleware/response-transformer.mjs';
 import ElasticBusinessBase from './lib/business/elastic-business-base.mjs';
 export default Framework;
 
-export { mssql, Azure, util, httpAuth, Elastic, adapters, logger, appConfig, lookup, sqlErrorMapper, BusinessBase, responseTransformer, ElasticBusinessBase };
+export { mssql, mysql, Azure, util, httpAuth, Elastic, adapters, logger, appConfig, lookup, sqlErrorMapper, BusinessBase, responseTransformer, ElasticBusinessBase };
