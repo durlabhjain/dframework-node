399d57d32d0623e9541b0132e14d0acadac2efdd
suyash pratap singh <suyash.singh@spraxa.com>
2025-06-05 19:31:38 +0530

    Review points by Dj sir

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import httpAuth from './lib/http-auth/index.js';
 import Elastic from './lib/elastic.js';
 import adapters from './lib/adapters/index.js';
 import logger from './lib/logger.js';
-import appConfig from './lib/appConfig.js';
+import appConfig from './lib/appConfig.mjs';
 import lookup from './lib/business/lookup.mjs';
 import { sqlErrorMapper } from './lib/business/error-mapper.mjs';
 import BusinessBase from './lib/business/business-base.mjs';
