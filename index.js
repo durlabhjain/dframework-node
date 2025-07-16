3f02ce3e9eb2ef95afc9329330aaa103a63ffaa7
suyash pratap singh <suyash.singh@spraxa.com>
2025-07-16 15:33:57 +0530

    65820: package upgrade removed dev dependencies

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 import Framework from './lib/index.js';
-import mssql from './lib/wrappers/mssql.js';
-import mysql from './lib/wrappers/mysql.js';
 import Azure from './lib/azure.js';
 import util from './lib/util.js';
 import httpAuth from './lib/http-auth/index.js';
@@ -16,4 +14,4 @@ import BusinessBaseRouter from './lib/business/business-objects.mjs';
 import responseTransformer from './lib/middleware/response-transformer.mjs';
 import ElasticBusinessBase from './lib/business/elastic-business-base.mjs';
 
-export { Framework, mssql, mysql, Azure, util, httpAuth, Elastic, adapters, logger, appConfig, lookup, sqlErrorMapper, BusinessBase, responseTransformer, ElasticBusinessBase, BusinessBaseRouter, Auth };
+export { Framework, Azure, util, httpAuth, Elastic, adapters, logger, appConfig, lookup, sqlErrorMapper, BusinessBase, responseTransformer, ElasticBusinessBase, BusinessBaseRouter, Auth };
